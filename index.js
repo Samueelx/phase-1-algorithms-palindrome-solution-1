ed6d6d60e806383bc59b144b11b2f8b9f36cae86
Samueelx <gsammygitau@gmail.com>
2022-12-30 18:57:34 +0300

    completed assignment.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  //reverse the input string
+  const reversedWord = reverseString(word);
+  //compare the reversed string to the input
+  return word === reversedWord;
+}
+
+function reverseString(word) {
+  //create an array from the input string
+  const wordArray = word.split("");
+  //reverse the array
+  const reversedArray = wordArray.reverse();
+  //create a string from the reversed array
+  const reversedWord = reversedArray.join("");
+  //return the reversed string
+  return reversedWord;
 }
 
 /* 
   Add your pseudocode here
+
+  Reverse the input string
+  if the reversed string is the same as the input
+    return true
+  else 
+    return false
 */
 
 /*
@@ -15,6 +35,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
